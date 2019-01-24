1cb337006664cb72aac78d6d675113d13519cf88
harishdd <46602963+harishdd@users.noreply.github.com>
2019-01-24 16:13:41 +0530

    Update odd_range.py

--- odd_range.py
+++ odd_range.py
@@ -1,5 +1,8 @@
 #harish
-N,K=map(int,input().split())
-for i in range(N,K+1):
-	if(i%2!=0):
-		print(i)
+N,Q=map(int,input().split())
+if(N<=100000):
+	for i in range(N,Q+1):
+		if(i%2!=0):
+			print(i)
+else:
+	print("invalid")
